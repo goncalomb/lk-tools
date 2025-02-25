b7675b307710d94b274a1dbdb9c8b90009c5a637
Gonçalo MB <me@goncalomb.com>
2025-02-25 21:46:49 +0000

    Update RPi kiosk script

--- rpi/rpi-lk7777-kiosk.sh
+++ rpi/rpi-lk7777-kiosk.sh
@@ -1,47 +1,53 @@
+#!/usr/bin/env bash
+
 # Playing video streams from Lenkeng HDMI over IP devices (version 4) on Raspberry Pi.
 
-# Tested on Raspberry Pi Zero 2 W, should work on other models too...
+# tested on a
+#   Raspberry Pi Zero 2 W Rev 1.0
+# with
+#   Raspberry Pi OS Lite (32-bit)
+# version
+#   Raspbian GNU/Linux 12 (bookworm)
 
 # https://github.com/goncalomb/lk-tools
 
+# compile ffmpeg with lk7777 support (see repository)
 # install Raspberry Pi OS Lite (32-bit)
 # configure network
-# compile ffmpeg with lk7777 support (see repository)
-# copy it to /home/pi/ffmpeg (make it executable)
-# copy the contents of this script to /etc/rc.local (change KEY=, see documentation on how to find it)
-# change dtoverlay on /boot/config.txt to dtoverlay=vc4-fkms-v3d
-# done
-# the video stream should start playing automatically on boot (after automatically installing dependencies)
+# copy ffmpeg to '/home/pi/ffmpeg'
+# copy this script to '/etc/rc.local' (change LK_KEY=, see repository on how to find it)
+# make it executable: sudo chmod +x /etc/rc.local
+# disable tty1 (optional): sudo systemctl disable getty@tty1
+# reboot
+# the first boot should take a while as it will install the required dependencies
+# the video stream should start playing automatically
 
-LK_FFMPEG="/home/pi/ffmpeg"
-LK_ADDRESS="udp://@239.255.42.42:7777"
-LK_KEY="00112233445566778899aabbccddeeff"
+set -euo pipefail
 
-# ./ffmpeg -demuxers 2>/dev/null | grep lk7777 >/dev/null
-echo "lk7777-kiosk: will start" >&2
-( (
-    set -e
+: "${LK_FFMPEG:="/home/pi/ffmpeg"}"
+: "${LK_ADDRESS:="udp://@239.255.42.42:7777"}"
+: "${LK_KEY:="00112233445566778899aabbccddeeff"}"
+: "${LK_USER:="pi"}"
+: "${LK_RETRY:=1}"
 
-    sleep 8
-    echo ; echo ; echo
+echo "waiting for network" >&2
+while [ -z "$(hostname -I)" ]; do sleep 2; done
 
-    echo "lk7777-kiosk: waiting for network" >&2
-    # dhcpcd -w -q
-    while [ -z "$(hostname -I)" ]; do sleep 2; done
+echo "$(hostname -I)" >&2
 
-    echo "lk7777-kiosk: addresses: $(hostname -I)" >&2
+if ! command -v cvlc >/dev/null; then
+    echo "installing vlc, this might take a while" >&2
+    apt -y update
+    apt -y install vlc-bin vlc-plugin-base
+fi
+
+echo "starting" >&2
+sudo -u "$LK_USER" bash <<EOF
+set -euo pipefail
+chmod +x "$LK_FFMPEG" || true
+while true; do
+    "$LK_FFMPEG" -timeout 1 -key "$LK_KEY" -i "$LK_ADDRESS" -codec copy -f mpegts - | cvlc --play-and-exit --no-osd - || [ "$LK_RETRY" == "1" ]
+    echo "player died, starting again" >&2
     sleep 2
-
-    if ! command -v cvlc >/dev/null; then
-        echo "lk7777-kiosk: installing vlc, this might take a while" >&2
-        apt -y update
-        apt -y install vlc-bin vlc-plugin-base
-    fi
-
-    echo "lk7777-kiosk: starting" >&2
-    sudo -u pi bash <<EOF
-"$LK_FFMPEG" -key "$LK_KEY" -i "$LK_ADDRESS" -vcodec copy -acodec copy -f mpegts - | cvlc -V mmal_vout -
+done;
 EOF
-)&)
-
-exit 0
